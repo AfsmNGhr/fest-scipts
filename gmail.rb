6fc24ad350805be6ff19aefdb3cff302bfe6b42a
Afay Zangetsu <afay.zangetsu@gmail.com>
2015-01-05 23:16:24 +0300

    fix ...

--- gmail.rb
+++ gmail.rb
@@ -1,87 +1,72 @@
 # coding: utf-8
+require 'net/ping'
 require 'gmail'
 require 'fest'
 require 'yaml'
 
-Gmail.new("login", "password") do |gmail|
+if Net::Ping::TCP.new('www.gmail.com', 'http').ping?
 
-  @fest = Fest.new
-  @labels = {"INBOX" => "Входящие", "Search job" => "Поиск работы",
-             "Music" => "Музыка", "Advertising" => "Реклама",
-             "Education" => "Обучение", "Interesting" => "Интересное"}
+  Gmail.new("login", "password") do |gmail|
 
-  def check_letters(gmail)
-    counts = {}
-    @labels.each do |k,v|
-      h = {k => gmail.mailbox(k).count(:unread)}
-      counts.merge!(h)
-    end
-    return counts
-  end
+    @fest = Fest.new
+    @labels = {"INBOX" => "Входящие", "Search job" => "Поиск работы",
+               "Music" => "Музыка", "Advertising" => "Реклама",
+               "Education" => "Обучение", "Interesting" => "Интер+есное",
+               "IT" => "Ай ти"}
 
-  def read_counts_letters
-    unless File.exist?(".gmail.yml")
-      return {}
-    end
-    old_counts = YAML::load(File.open(".gmail.yml"))
-    return old_counts
-  end
-
-  def save_counts_letters(counts)
-    system("touch .gmail.yml") if File.exist?(".gmail.yml")
-    File.open(".gmail.yml", "w") do |f|
-      f.write counts.to_yaml
-    end
-  end
-
-  def check_new_counts_letters(counts, old_counts)
-    counts.each do |k, v|
-      if old_counts.empty? || v < old_counts[k]
-        count = v
-      else
-        count = v - old_counts[k]
+    def check_counts_letters(gmail)
+      counts = {}
+      @labels.each do |k,v|
+        h = {k => gmail.mailbox(k).count(:unread)}
+        counts.merge!(h)
       end
-      say_new_counts(k, count)
+      return counts
     end
-  end
 
-  def say_new_counts(k, count)
-    unless count == 0
-      text = pluralform(count)
-      count = "Одн+о" if count == 1
-      all = "У вас #{count} #{text}"
-      part = "#{count} #{text} в разделе #{@labels[k]}"
+    def read_old_counts_letters
+      return {} unless File.exist?(".gmail.yml")
+      old_counts = YAML::load(File.open(".gmail.yml"))
+    end
 
-      if k == "INBOX"
-        @fest.say(all)
-      else
-        @fest.say(part)
+    def save_counts_letters(counts)
+      system("touch .gmail.yml") if File.exist?(".gmail.yml")
+      File.open(".gmail.yml", "w") do |f|
+        f.write counts.to_yaml
       end
     end
-  end
 
-  def pluralform(count)
-    n = count % 100
-    m = n % 10
-    msg = ['сообщение',
-           'сообщения',
-           'сообщений']
-
-    if n > 10 && n < 20
-      return msg[2]
-    elsif m > 1 && m < 5
-      return msg[1]
-    elsif m == 1
-      return msg[0]
-    else
-      return msg[2]
+    def check_new_counts_letters(counts, old_counts)
+      counts.each do |k, v|
+        if old_counts.empty? || v < old_counts[k]
+          count = v
+        else
+          count = v - old_counts[k]
+        end
+        say_new_counts(k, count)
+      end
     end
-  end
 
-  counts = check_letters(gmail)
-  old_counts = read_counts_letters
-  check_new_counts_letters(counts, old_counts)
-  save_counts_letters(counts)
-  gmail.logout
+    def say_new_counts(k, count)
+      unless count == 0
+        text = @fest.pluralform(count, ['сообщение',
+                                        'сообщения',
+                                        'сообщений'])
+        count = "Одн+о" if count == 1
+
+        if k == "INBOX"
+          @fest.say("У вас #{count} #{text}")
+        else
+          @fest.say("#{count} #{text} в разделе #{@labels[k]}")
+        end
+      end
+    end
+
+    counts = check_counts_letters(gmail)
+    old_counts = read_old_counts_letters
+    check_new_counts_letters(counts, old_counts)
+    save_counts_letters(counts)
+    gmail.logout
+
+  end
 
 end
